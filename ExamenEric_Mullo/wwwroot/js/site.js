34c33c319c81a331c4df1a53d23a0d5b073ba5f3
ericmullo <ericrodrigomullo@gmail.com>
2024-10-30 11:21:29 -0500

    Boton agregado desde el JS a la pantalla principal

--- ExamenEric_Mullo/wwwroot/js/site.js
+++ ExamenEric_Mullo/wwwroot/js/site.js
@@ -1,4 +1,11 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+/mnt/data / ExamenEric_Mullo - master / ExamenEric_Mullo - master / ExamenEric_Mullo / wwwroot / js / site.js
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("mostrarMensaje").addEventListener("click", function () {
+        alert("Hola soy Eric Mullo y mi Hobbie es jugar fútbol");
+    });
+});
+
 
 // Write your JavaScript code.
