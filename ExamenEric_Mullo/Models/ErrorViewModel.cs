df9937e4ba33c784bdec307211511bd53a79ddf7
laboratorio <laboratorio@UPOAULA10319>
2024-10-30 08:23:26 -0500

    Agregar archivos de proyecto.

---
+++ ExamenEric_Mullo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamenEric_Mullo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
