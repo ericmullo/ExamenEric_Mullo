4a084ff569f99b506bffb21816c6e9cdb755f791
ericmullo <ericrodrigomullo@gmail.com>
2024-10-30 08:42:42 -0500

    Empezando la creacion de modelos

---
+++ ExamenEric_Mullo/Models/Celular.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExamenEric_Mullo.Models
+{
+    public class Celular
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(20)]
+        public string Modelo { get; set; }
+        [Range(2000, 2024)]
+        public anio
+    }
+}
